0037ac1e843892320b8b3f43df99eeed5decc8fa
WesleyBule <bulewesley7@gmail.com>
2026-06-20 22:13:45 +0200

    Spring Boot initialized!

---
+++ src/main/java/com/fandza/ccs/Fandza/FandzaApplication.java
@@ -0,0 +1,13 @@
+package com.fandza.ccs.Fandza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FandzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FandzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fandza/ccs/Fandza/FandzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fandza.ccs.Fandza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FandzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
